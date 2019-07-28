6957224d36564e7d3e20af3bff42a18e6c0a749c
Aleksandr Barmin <barmin.alexander@gmail.com>
2019-07-28 19:54:50 +0300

    07. SOLID SRP, fixed

--- src/main/java/com/epam/trainogram/service/friend/FriendshipFacadeImpl.java
+++ src/main/java/com/epam/trainogram/service/friend/FriendshipFacadeImpl.java
@@ -1,18 +1,20 @@
 package com.epam.trainogram.service.friend;
 
-import com.epam.trainogram.domain.Friendship;
 import com.epam.trainogram.domain.User;
+import com.epam.trainogram.domain.notification.NewFriendshipNotification;
 import com.epam.trainogram.service.notification.NotificationService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class FriendshipServiceImpl implements FriendshipService {
-  private final FriendshipDao friendshipDao;
+public class FriendshipFacadeImpl implements FriendshipFacade {
+  private final FriendshipService friendshipService;
   private final NotificationService notificationService;
 
   @Override
   public void addToFriends(User owner, User newFriend) {
-    friendshipDao.save(new Friendship(owner, newFriend));
-    friendshipDao.save(new Friendship(newFriend, owner));
+    friendshipService.addToFriends(owner, newFriend);
+    notificationService.sendNotification(
+        new NewFriendshipNotification(owner, newFriend)
+    );
   }
 }
